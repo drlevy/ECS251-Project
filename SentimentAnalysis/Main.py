f2979a9048429cb409c979b778205f054485fdba
Daniel R. Levy <daniel.levy@gmail.com>
2016-06-13 01:10:20 -0700

    printing out sentiment scores

--- SentimentAnalysis/Main.py
+++ SentimentAnalysis/Main.py
@@ -2,18 +2,18 @@ import sys
 from SentimentAnalysis.Controller.SentimentProcessor import SentimentProcessor
 from SentimentAnalysis.Controller.GraphGenerator import GraphGenerator
 from SentimentAnalysis.Controller.DomainController import DomainController
+from SentimentAnalysis.Controller.StatisticsGenerator import StatisticsGenerator
 
 
 def main(args):
     dc = DomainController()
     sp = SentimentProcessor()
     gg = GraphGenerator()
+    sg = StatisticsGenerator()
 
     sp.analyze_and_set_sentiments()
-    print('aggregate sentiment score: ' + str(dc.aggregate_sentiment))
 
-    gg.generate_graph()
-    print('graph generated ...')
+    sg.print_sentiments()
 
     sys.exit(0)
 
